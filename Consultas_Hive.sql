f6a433bd322f421f00e1957fc0149bcc698526d7
Elizabeth11 <semartine1@eafit.edu.co>
2023-03-06 11:05:54 -0500

    Hive con Consultas

--- Consultas_Hive.sql
+++ Consultas_Hive.sql
@@ -10,3 +10,23 @@ Municipio STRING,ZonaHidrografica STRING, Latitud FLOAT,Longitud FLOAT, Descripc
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' 
 STORED AS TEXTFILE 
 LOCATION 's3://trabajo-1/raw/temperatura/'
+-- Consulta para datos de temperatura
+SELECT replace(temp.departamento, '"', '') as departamento,
+	temp.ano as ano,
+	avg(temp.valorobservado) as temperatura
+FROM trusted_temperatura as temp
+WHERE ano = 2021
+group by temp.ano, temp.departamento
+order by temperatura desc
+limit 10;
+-- Fin de consulta
+-- Consulta para datos de presion 
+SELECT replace(temp.municipio, '"', '') as municipio,
+	temp.ano as ano,
+	avg(temp.valorobservado) as presion_atmosferica
+FROM trusted_presion_atmosferica as temp
+WHERE temp.ano = 2021
+group by temp.ano, municipio
+order by presion_atmosferica desc
+limit 10;
+-- fin de consulta
